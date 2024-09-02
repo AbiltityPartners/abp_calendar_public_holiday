b6d8eeccb9068e63b51cf2fe9c67b82e56cfe12d
“devgembaware” <dev@gembaware.com>
2024-09-02 10:08:28 +0200

    [ADD] abp_calendar_public_holiday: add new module

---
+++ abp_calendar_public_holiday/models/calendar_event.py
@@ -0,0 +1,28 @@
+# -- coding: utf-8 --
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class CalendarEvent(models.Model):
+    _inherit = 'calendar.event'
+    
+    abp_resource_calendar_leaves = fields.Many2one('resource.calendar.leaves', string='Public Holiday', readonly=True, ondelete='cascade')
+
+    def write(self, vals):
+        if self.abp_resource_calendar_leaves:
+            for field in self._get_leaves_readonly_fields():
+                if field in vals:
+                    raise UserError(_('It is not allowed to modify the dates of a public holiday, please modify it from the public holiday record'))
+        return super().write(vals)
+
+    @api.model
+    def _get_leaves_readonly_fields(self):
+        return [
+            'start',
+            'stop',
+            'duration',
+            'allday',
+            'start_date',
+            'stop_date'
+        ]
