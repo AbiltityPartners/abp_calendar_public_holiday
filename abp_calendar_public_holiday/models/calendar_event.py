4d21af7d09fc36fedd321a123c847085d7c42965
“devgembaware” <dev@gembaware.com>
2024-09-02 11:37:10 +0200

    [REF] abp_calendar_public_holiday: allow to modify event only from public holiday

--- abp_calendar_public_holiday/models/calendar_event.py
+++ abp_calendar_public_holiday/models/calendar_event.py
@@ -10,11 +10,10 @@ class CalendarEvent(models.Model):
     abp_resource_calendar_leaves = fields.Many2one('resource.calendar.leaves', string='Public Holiday', readonly=True, ondelete='cascade')
 
     def write(self, vals):
-        if self.abp_resource_calendar_leaves:
-            if not self.env.context.get('allow_to_change_event'):
-                for field in self._get_leaves_readonly_fields():
-                    if field in vals:
-                        raise UserError(_('It is not allowed to modify the dates of a public holiday, please modify it from the public holiday record'))
+        if self.abp_resource_calendar_leaves not self.env.context.get('allow_to_change_event'):
+            for field in self._get_leaves_readonly_fields():
+                if field in vals:
+                    raise UserError(_('It is not allowed to modify the dates of a public holiday, please modify it from the public holiday record'))
         return super().write(vals)
 
     @api.model
